b0aa85c9eb0de4977e26df66df7bab69486219fd
Roland Laurès <roland.laures@gnuside.com>
2014-02-18 10:07:27 +0100

    created a safe_mkdir

--- WURFLCap.php
+++ WURFLCap.php
@@ -7,6 +7,8 @@ define('WURFL_PERSISTENCE_DIR', WURFL_RESOURCES_DIR . '/storage/persistence');
 define('WURFL_CACHE_DIR',       WURFL_RESOURCES_DIR . '/storage/cache');
 define('WURFL_DIR',             WURFL_PLUGIN_DIR . '/WURFL');
 
+include_once('WURFL/FileUtils.php');
+
 
 class WURFLCap {
     var $base;
@@ -19,54 +21,53 @@ class WURFLCap {
         register_activation_hook( $this->base, array( $this, 'activate' ) );
         register_deactivation_hook( $this->base, array( $this, 'deactivate' ) );
         register_uninstall_hook( $this->base, array( $this, 'uninstall' ) );
-        
+
         add_action( 'setup_theme', array( $this, 'action_setup_theme') );
     } // end constructor
 
     /**
      * Fired when the plugin is activated.
      *
-     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
      */
     public function activate( $network_wide ) {
         $new_file_path = WURFL_BASE_DIR;
-        
+
         if (!file_exists(WURFL_PLUGIN_DIR.'/wurfl.zip')){
             wp_die("wurfl.zip file not found");
         }
 
-        if (!file_exists($new_file_path)) {
-            if (!mkdir($new_file_path, 0755, true)){
+        if (!safe_mkdir($new_file_path, 0755, true)){
                 wp_die("Permission denied, make sure you have write permission to wp-content folder.");
                 // return array(
                     // 'error' => 'Permission denied, make sure you have write permission to wp-content folder.'
                 // );
             }
         }
-        
+
         $persistence = WURFL_PERSISTENCE_DIR;
         $cache       = WURFL_CACHE_DIR;
-        
-        if (!mkdir($persistence, 0755, true)) {
+
+        if (!safe_mkdir($persistence, 0755, true)) {
             wp_die('Permission denied, make sure you have write permission to '.$new_file_path.' folder.');
             // return array(
                 // 'error' => 'Permission denied, make sure you have write permission to '.$new_file_path.' folder.'
             // );
         }
-        
-        if (!mkdir($cache, 0755, true)) {
+
+        if (!safe_mkdir($cache, 0755, true)) {
             wp_die('Permission denied, make sure you have write permission to '.$new_file_path.' folder.');
             // return array(
                 // 'error' => 'Permission denied, make sure you have write permission to '.$new_file_path.' folder.'
             // );
         }
-        
+
     } // end activate
 
     /**
      * Fired when the plugin is deactivated.
      *
-     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
      */
     public function deactivate( $network_wide ) {
     } // end deactivate
@@ -74,14 +75,14 @@ class WURFLCap {
     /**
      * Fired when the plugin is uninstalled.
      *
-     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+     * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
      */
     public function uninstall( $network_wide ) {
         $new_file_path = WURFL_BASE_DIR;
         $this->recursive_remove_directory($new_file_path);
     } // end uninstall
 
-    
+
     // code borrowed from http://lixlpixel.org/recursive_function/php/recursive_directory_delete/
     public function recursive_remove_directory($directory, $empty=FALSE) {
         // if the path has a slash at the end we remove it here
@@ -140,7 +141,7 @@ class WURFLCap {
             return TRUE;
         }
     }
-    
+
     public function action_setup_theme () {
         require_once WURFL_DIR .'/Application.php';
 
@@ -171,34 +172,34 @@ class WURFLCap {
         // Create a WURFL Manager
         /* @var $wurflManager WURFL_WURFLManager */
         $wurflManager = $wurflManagerFactory->create();
-        
+
         $this->requestingDevice = $wurflManager->getDeviceForHttpRequest($_SERVER);
     }
-    
+
     public function getCapability ($cap = false) {
         return $this->requestingDevice->getCapability($cap);
     }
-    
+
     public function is_wireless_device () {
         return $this->getCapability('is_wireless_device');
     }
-    
+
     public function is_tablet () {
         return $this->getCapability('is_tablet');
     }
-    
+
     public function is_touch () {
         return $this->pointing_method() == "touchscreen" ? "true" : "false";
     }
-    
+
     public function supports_borderradius () {
         return $this->getCapability('css_rounded_corners') == "css3";
     }
-    
+
     public function supports_gradient () {
         return $this->getCapability('css_gradient') == "css3";
     }
-    
+
     public function pointing_method () {
         return $this->getCapability('pointing_method');
     }
